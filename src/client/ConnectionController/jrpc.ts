b8d1dd17d48ba75c915ab3630cfa04df53661be4
Ivan Kalinin <i.kalinin@dexpa.io>
2022-08-09 15:13:27 +0200

    Add support for local JRPC node

--- src/client/ConnectionController/jrpc.ts
+++ src/client/ConnectionController/jrpc.ts
@@ -8,8 +8,14 @@ const { nekoton, fetch } = core;
  * @category Client
  */
 export type JrpcSocketParams = {
-  // Path to jrpc api endpoint
-  endpoint: string
+  /**
+   * Full JRPC API endpoint
+   */
+  endpoint: string;
+  /**
+   * Alternative JRPC API that will be used for broadcasting messages or fetching states
+   */
+  alternativeEndpoint?: string;
 }
 
 export class JrpcSocket {
@@ -18,16 +24,20 @@ export class JrpcSocket {
     params: JrpcSocketParams,
   ): Promise<nt.JrpcConnection> {
     class JrpcSender {
-      private readonly params: JrpcSocketParams;
+      private readonly endpoint: string;
+      private readonly alternativeEndpoint: string;
 
       constructor(params: JrpcSocketParams) {
-        this.params = params;
+        this.endpoint = params.endpoint;
+        this.alternativeEndpoint = params.alternativeEndpoint != null
+          ? params.alternativeEndpoint
+          : params.endpoint;
       }
 
-      send(data: string, handler: nt.JrpcQuery) {
+      send(data: string, handler: nt.JrpcQuery, requiresDb: boolean) {
         ;(async () => {
           try {
-            const response = await fetch(this.params.endpoint, {
+            const response = await fetch(requiresDb ? this.endpoint : this.alternativeEndpoint, {
               method: 'post',
               headers: {
                 'Content-Type': 'application/json',
